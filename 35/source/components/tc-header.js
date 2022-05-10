fec2f5913dab6d7647237ec683886813cbb0af72
tc-kyano <k.yano@timeconcier.jp>
2022-05-10 14:15:36 +0900

    urlからパラメータを取得する関数を共通部分に移行

--- 35/source/components/tc-header.js
+++ 35/source/components/tc-header.js
@@ -3,10 +3,34 @@ Vue.component('tc-header', {
 	props: ['client', 'aryMasterRecord'],
 	data() {
 		return {
-
+			isAdminOpen: true,
+			group: '',
+			ssect: '',
+			scost: 0,
+			officeInfoRecId: 0,
+			officeName: ''
 		}
 	},
-	template: `
+	async mounted() {
+		// カスタマイズ一覧の選択等を非表示にする
+		const el = document.getElementsByClassName('contents-actionmenu-gaia')[0];
+		el.style.display = 'none';
 
+		this.ssect = getParam('ssect');
+		this.scost = Number(getParam('scost'));
+		this.group = getParam('officeGroup');
+	},
+	template: `
+		<div>
+			<tc-param
+				v-show="isAdminOpen"
+				:group="group"
+				:ssect="ssect"
+				:scost="scost"
+				:office-info-rec-id="officeInfoRecId"
+				:office-name="officeName"
+			>
+			</tc-param>
+		</div>
 	`
 });
